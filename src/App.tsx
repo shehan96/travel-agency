a45caf21602032a3cc4bb5de3b5d4a56a3ad2cb5
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-12-01 14:59:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Carousel } from './components';
+import { Home } from './pages';
+import { StrictMode } from 'react';
+import { Header } from './components';
 
 function App() {
-  return <Carousel />;
+  return (
+    <StrictMode>
+      <Header />
+      <Home />
+    </StrictMode>
+  );
 }
 
 export default App;
