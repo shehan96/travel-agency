f1b1f75a68e3d7eaf112223fd862ecc7826f8d57
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-12-24 19:41:04 +0530

    Create Destinations.tsx

---
+++ src/components/Destinations/Destinations.tsx
@@ -0,0 +1,8 @@
+import { Box } from '@chakra-ui/react';
+import { sampleImages } from '../../data';
+
+const Destinations = () => {
+  <Box backgroundImage={sampleImages.destinations.img1}></Box>;
+};
+
+export { Destinations };
